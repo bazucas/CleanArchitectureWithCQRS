2dddccf257f0cfba0e936569d90289542eaef557
Luís Inácio <lrs.inacio@gmail.com>
2024-08-18 18:32:31 +0100

    Email servicem Sql connection and Datetime interfaces added

---
+++ Bookify.Application/Abstractions/Email/IEmailService.cs
@@ -0,0 +1,8 @@
+﻿using EmailAddress = Bookify.Domain.Users.Email;
+
+namespace Bookify.Application.Abstractions.Email;
+
+public interface IEmailService
+{
+    Task SendAsync(EmailAddress recipient, string subject, string body);
+}
